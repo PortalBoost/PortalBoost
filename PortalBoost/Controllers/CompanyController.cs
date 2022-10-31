60da87e769d8f955967feb82bf8cbe5454d0a871
Jesper Eriksson <89935390+jes8eri@users.noreply.github.com>
2022-10-31 16:45:49 +0100

    Re-added accidentally removed nuget..

--- PortalBoost/Controllers/CompanyController.cs
+++ PortalBoost/Controllers/CompanyController.cs
@@ -1,8 +1,8 @@
 ﻿namespace PortalBoost.Controllers
 {
-using Microsoft.AspNetCore.Mvc;
-using PortalBoost.Data.Models;
-using PortalBoost.Data.Services;
+    using Microsoft.AspNetCore.Mvc;
+    using PortalBoost.Data.Models;
+    using PortalBoost.Data.Services;
     using System;
     using System.Collections.Generic;
     using System.Linq;
