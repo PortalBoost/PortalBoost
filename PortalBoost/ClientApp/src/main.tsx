ef82877e8a195f76c758a447b7fff6a100cb5b9b
Jesper Eriksson <89935390+jes8eri@users.noreply.github.com>
2022-11-02 10:08:16 +0100

    Added fonts and font-styling

--- PortalBoost/ClientApp/src/main.tsx
+++ PortalBoost/ClientApp/src/main.tsx
@@ -7,7 +7,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RecoilRoot>
-    <App/>
+      <App />
     </RecoilRoot>
   </React.StrictMode>
 )
