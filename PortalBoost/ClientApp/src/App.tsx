cf65f67e7372048c733fd0f011e3c3e6171d6261
Jesper Eriksson <89935390+jes8eri@users.noreply.github.com>
2022-11-02 12:48:58 +0100

    Emtpy pages

--- PortalBoost/ClientApp/src/App.tsx
+++ PortalBoost/ClientApp/src/App.tsx
@@ -1,22 +1,29 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import ContentWrapper from './components/common/ContentWrapper';
 import LandingPage from './pages/landingPage/LandingPage';
 import PageNotFound from './pages/pageNotFound/PageNotFound';
+import ViewEmployees from './pages/viewEmployees/ViewEmployees';
+import ViewCompanies from './pages/viewCompanies/ViewCompanies';
 
 
 function App() {
 
   return (
     <>
-      <h1 className='text-6xl'>Yeet</h1>
+
       <Router>
+
+        <h1 className='text-6xl'>
+          <Link to="/">Yeet</Link>
+        </h1>
 
         {/* Header */}
         <ContentWrapper>
 
           <Routes>
             {/* If logged in, show LandingPage, else show LoginPage */}
+            <Route path="employees" element={<ViewEmployees />} />
+            <Route path="companies" element={<ViewCompanies />} />
             <Route path='/' element={<LandingPage />} />
             <Route path="*" element={<PageNotFound />} />
 
