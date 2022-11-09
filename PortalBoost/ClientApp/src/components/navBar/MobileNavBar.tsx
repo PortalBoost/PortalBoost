8f5c0fc7e1c893ab872615be4f209858b70bf9a0
TiiaTu <tiia.tupamaki@gmail.com>
2022-11-09 16:01:43 +0100

    small fixes to css

--- PortalBoost/ClientApp/src/components/navBar/MobileNavBar.tsx
+++ PortalBoost/ClientApp/src/components/navBar/MobileNavBar.tsx
@@ -1,27 +1,40 @@
-import {RiSuitcaseLine} from 'react-icons/ri'
-import {AiOutlineTeam} from 'react-icons/ai'
+import { RiSuitcaseLine } from 'react-icons/ri'
+import { AiOutlineTeam } from 'react-icons/ai'
+import { AiOutlineHome } from 'react-icons/ai'
+import { Link } from 'react-router-dom'
 
 const MobileNavBar: React.FC<{}> = () => {
+
+	const navBarItem = "hover:text-n-turquoise-dark grid justify-items-center"
 
 	return (
 		<div className="fixed bottom-0 flex 
 		 dark:bg-n-gray shadow-xl">
-			<div className=" text-black border-pink-700">
-				
-				<ul className=" flex justify-around p-2 w-screen ">
-					
-					{/* länk till personsida */}
-					<li className="hover:text-n-turquoise-dark  border-pink-600 grid justify-items-center">
-						<p><AiOutlineTeam /></p>
-						<p>medarbetare</p>
-					</li>
+			<div className=" text-black">
 
-					{/* länk till companysida */}
-					<li className="hover:text-n-turquoise-dark grid justify-items-center">
-						<p><RiSuitcaseLine /></p>
-						<p>kunder</p>
-					</li>
-				
+				<ul className=" flex justify-around p-2 w-screen ">
+
+					<Link to="/">
+						<li className={navBarItem}>
+							<p><AiOutlineHome /></p>
+							<p>home</p>
+						</li>
+					</Link>
+
+					<Link to="/employees">
+						<li className={navBarItem}>
+							<p><AiOutlineTeam /></p>
+							<p>medarbetare</p>
+						</li>
+					</Link>
+
+					<Link to="/companies">
+						<li className={navBarItem}>
+							<p><RiSuitcaseLine /></p>
+							<p>kunder</p>
+						</li>
+					</Link>
+
 				</ul>
 			</div>
 		</div>
--- PortalBoost/ClientApp/src/components/navBar/Navbar.tsx
+++ PortalBoost/ClientApp/src/components/navBar/Navbar.tsx
@@ -1,28 +1,55 @@
-import {BsPerson} from 'react-icons/bs'
-import { useState} from 'react'
+import { RiSuitcaseLine } from 'react-icons/ri'
+import { AiOutlineTeam } from 'react-icons/ai'
+import { AiOutlineHome } from 'react-icons/ai'
+import { IoLogOutOutline } from 'react-icons/io5'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar: React.FC<{}> = () => {
 
-const [isOpen, setIsOpen] = useState(true)
+	//TODO: use atom instead of state
+	const [isOpen, setIsOpen] = useState(true)
+
+	const navBarItem = "hover:text-n-turquoise-dark ease-in-out duration-300 hover:cursor-pointer flex justify-start gap-4 items-center"
 
 
 	return (
-		<div className="fixed top-0 left-0 h-screen w-30s flex flex-col
+		<div className="fixed top-0 left-0
 		bg-white dark:bg-n-gray shadow-lg">
-		  
-		<div className="mt-40 text-white p-4">
-		<ul className="hover:text-n-purple gap-4">
-			{/* länk till personsida */}
-			<li>
-				medarbetare
-			</li>
-			{/* länk till companysida */}
-			<li>
-				kunder
-			</li>
-			</ul>	
+
+			<div className="mt-20 pt-6 m-2 text-black p-3 h-screen flex items-start">
+				<ul className="flex flex-col justify-evenly gap-4">
+
+					<Link to="/">
+						<li className={navBarItem}>
+							<p><AiOutlineHome /></p>
+							<p>home</p>
+						</li>
+					</Link>
+
+					<Link to="/employees">
+						<li className={navBarItem}>
+							<p><AiOutlineTeam /></p>
+							<p>medarbetare</p>
+						</li>
+					</Link>
+
+					<Link to="/companies">
+						<li className={navBarItem}>
+							<p><RiSuitcaseLine /></p>
+							<p>kunder</p>
+						</li>
+					</Link>
+				</ul>
+
+				{/* TODO: add functionality to log out*/}
+				{/* log out button */}
+				<div className="fixed bottom-2 hover:text-n-turquoise-dark ease-in-out duration-300 hover:cursor-pointer flex justify-start gap-4 items-center">
+					<p><IoLogOutOutline /></p>
+					<p>Log out</p>
+				</div>
+			</div>
 		</div>
-</div>
 	)
 }
 
