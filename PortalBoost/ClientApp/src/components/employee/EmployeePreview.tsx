3327849949848e9b88df1fb4a86ad46bab7d33ef
Jesper Eriksson <89935390+jes8eri@users.noreply.github.com>
2022-11-09 14:22:58 +0100

    Grid WIP

--- PortalBoost/ClientApp/src/components/employee/EmployeePreview.tsx
+++ PortalBoost/ClientApp/src/components/employee/EmployeePreview.tsx
@@ -18,18 +18,21 @@ const EmployeePreview = () => {
 	// TODO: Nicer looking outline
 	// TODO: Hover effects
 	// TODO: Overlays?
-	const temppic = <div className="bg-sky-300 w-[600px] h-[300px] p-1 m-1 shrink flex justify-center items-center ">Placeholder Picture</div>
+	// TODO: Crop image
+	const temppic = <div className="bg-n-purple-dark w-[600px] h-[300px] p-1 m-1 shrink flex justify-center items-center rounded-lg
+	 bg-gradient-to-br opacity-50 from-n-offwhite ">Placeholder Picture</div>
 
 	return (
-		<div className="h-auto rounded-lg flex flex-col bg-white shadow-lg gap-1 ring-1 ring-n-gray">
+		<div className="h-auto rounded-lg flex flex-col bg-white shadow-lg gap-1 ring-1 ring-n-gray md:max-w-md
+		transition-all hover:cursor-pointer hover:-translate-y-2 hover:shadow-xl">
 
 			{/** Fake image */}
-			<div className="flex justify-center shrink">
+			<div className="flex justify-center ">
 				{temppic}
 			</div>
 			<hr className="my-1"></hr>
 			<div className="p-4 pt-2">
-				<div className="text-n-purple font-semibold text-lg">Persons Name</div>
+				<div className="text-n-purple font-semibold text-lg">Person Name</div>
 				<p className="mt-2 font-bold">One-liner</p>
 				<p className="">Short intro stuff, interesting things in here. Short and sweet. </p>
 			</div>
--- PortalBoost/ClientApp/src/pages/viewEmployees/ViewEmployees.tsx
+++ PortalBoost/ClientApp/src/pages/viewEmployees/ViewEmployees.tsx
@@ -4,17 +4,17 @@ const ViewEmployees = () => {
 
 	//TODO: Get all employees/mock employees. Map out all employees in container, show employeepreview for each employee. On click, get id, show modal with usermodal with same id.
 
-	const AMOUNT_OF_TEST_EMPLOYEES = [1, 2, 3, 4, 5]
+	const AMOUNT_OF_TEST_EMPLOYEES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 	return (
-		<div className="container mt-20 bg-slate-500 p-5 min-h-screen">
+		<div className="container mt-20 p-5 ">
 			{/** Test container */}
+			<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5">
+				{AMOUNT_OF_TEST_EMPLOYEES.map((x) =>
+					<EmployeePreview key={x} />
+				)}
 
-			{/* {AMOUNT_OF_TEST_EMPLOYEES.map((x) =>
-					<div key={x}>Employee</div>
-				)} */}
-			<EmployeePreview />
-
+			</div>
 		</div>
 	)
 }
