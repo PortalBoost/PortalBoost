9d93d4cee2cdcd9c8ceb00fcc4d3f464830373ff
Jesper Eriksson <89935390+jes8eri@users.noreply.github.com>
2022-11-11 10:34:47 +0100

    Grid changes

--- PortalBoost/ClientApp/src/pages/viewEmployees/ViewEmployees.tsx
+++ PortalBoost/ClientApp/src/pages/viewEmployees/ViewEmployees.tsx
@@ -32,7 +32,7 @@ const ViewEmployees = () => {
 				</div>
 
 				{/** Test container */}
-				<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
+				<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 gap-5">
 					{TEST_EMPLOYEES.map((x) => (
 						<EmployeePreview key={x.testEmployeeID} testId={x.testEmployeeID} />
 					))}
