28489f0aa246fce8d6aa47ed09d8b4f8fb7aba0c
Jesper Eriksson <89935390+jes8eri@users.noreply.github.com>
2022-11-11 11:15:10 +0100

    .

--- PortalBoost/ClientApp/src/pages/viewEmployees/ViewEmployees.tsx
+++ PortalBoost/ClientApp/src/pages/viewEmployees/ViewEmployees.tsx
@@ -35,7 +35,7 @@ const ViewEmployees = () => {
 					<p className="text-2xl ">Meet the MyTeam Team!</p>
 					<div className="bg-gradient-to-r from-n-dark p-[1px] max-w-xs rounded-full"></div>
 					<p className="pt-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure fugiat animi quidem aut voluptate eius voluptas ad ipsa officia</p>
-					<p className="pt-2">Nånting kul om fika. Nånting om att ta kontakt via mail. </p>
+					<p className="pt-2">Entertaining sentence about having a fika. Sentence about getting in touch via mail. </p>
 				</div>
 				{/** Test container */}
 				<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 gap-5">
