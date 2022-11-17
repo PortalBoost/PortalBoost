ee33caedfe4d88727152d78b2b2b7328e1221822
Jesper Eriksson <89935390+jes8eri@users.noreply.github.com>
2022-11-17 19:31:31 +0100

    .

--- PortalBoost/ClientApp/src/components/employee/EmployeePreviewRounded.tsx
+++ PortalBoost/ClientApp/src/components/employee/EmployeePreviewRounded.tsx
@@ -11,14 +11,16 @@ const EmployeePreviewRounded = ({ employee }: { employee: EmployeeModel }) => {
 		setOpenModal(!openModal)
 	}
 
-
-	const placeholderImage = <div className="from-n-dark bg-gradient-to-br  p-10 mx-auto "> <AiOutlineUser className="text-8xl text-n-offwhite filter opacity-90 drop-shadow-[4px_4px_4px_black]" /> </div>
+	//from-n-dark to-slate-600 bg-gradient-to-br 
+	const placeholderImage = <div className=" from-n-dark to-slate-600 bg-gradient-to-br  p-10 mx-auto "> <AiOutlineUser className="text-8xl text-n-offwhite filter opacity-90 drop-shadow-[4px_4px_4px_black]" /> </div>
 	//transition-all hover:cursor-pointer hover:-translate-y-2 hover:shadow-xl
+
+	// TODO: Border thingy top right, bottom left opposite side. Animate?
 	return (
 		<>
 			<div className=" flex flex-col relative group ">
 
-				<div onClick={toggleOpen} className="bg-slate-600 rounded-full max-w-[176px] max-h-[176px] mx-auto overflow-clip ring-2 ring-n-dark cursor-pointer" >
+				<div onClick={toggleOpen} className=" rounded-full max-w-[176px] max-h-[176px] mx-auto overflow-clip ring-2 ring-n-dark cursor-pointer" >
 					{placeholderImage}
 				</div>
 
