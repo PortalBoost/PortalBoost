83e2e5a1cddfc05658a7c63206a87b4c67f49563
TiiaTu <tiia.tupamaki@gmail.com>
2022-11-18 09:00:15 +0100

    basic structure for companypage

--- PortalBoost/ClientApp/src/pages/viewCompanies/ViewCompanies.tsx
+++ PortalBoost/ClientApp/src/pages/viewCompanies/ViewCompanies.tsx
@@ -20,7 +20,7 @@ const ViewCompanies = () => {
 		{companyId: 1, companyName: "Company 1", companyDescription: "This is a company. A fine one. Not a regular company, but a company still. Company with a goal. What more do you want to know? ... wfaofjaweiofjaweifjaoeifjaoiegfaoifjaoifjaoiejfaiojefoaijeföoiajefioajeöfiojaeöiofjaoiefjaöoiejfaeöiofjaöeoifjaöoiefjaöoiergjaeöoifja"},
 		{companyId: 2, companyName: "Company 2", companyDescription: "This is a another fine company"},
 		{companyId: 3, companyName: "Company 3", companyDescription: "Yet another company! Wish to know more? Click"},
-		{companyId: 4, companyName: "Company 4", companyDescription: "One more. Here you can find info if you dare"},
+		{companyId: 4, companyName: "Company 4", companyDescription: "One more. Here you can find info"},
 		{companyId: 5, companyName: "Company 5", companyDescription: "Company. company. company. company. company. and company."},
 	]
 
